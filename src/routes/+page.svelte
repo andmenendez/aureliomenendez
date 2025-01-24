d7e859ff87188fd1bcec85319422c57bd3a05c29
Andres M Menendez <50599165+andmenendez@users.noreply.github.com>
2025-01-24 08:53:06 -0500

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="grid place-items-center">  
+  <h1>test</h1>
+  <h2>test</h2>
+</div>
