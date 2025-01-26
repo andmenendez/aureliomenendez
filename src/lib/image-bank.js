bdffa6573ebb5bd4b6035fa1e9032812d04d0ec1
Andres M Menendez <50599165+andmenendez@users.noreply.github.com>
2025-01-25 19:58:43 -0500

    fix image names

--- src/lib/image-bank.js
+++ src/lib/image-bank.js
@@ -1,144 +1,168 @@
-import asturias1 from "../lib/assets/albums/asturias/a.jpeg"
+import asturias1 from "$lib/assets/albums/asturias/a.jpeg";
 import asturias2 from "$lib/assets/albums/asturias/b.jpeg";
 import asturias3 from "$lib/assets/albums/asturias/c.jpeg";
 import asturias4 from "$lib/assets/albums/asturias/d.jpeg";
 import asturias5 from "$lib/assets/albums/asturias/e.jpeg";
 import asturias6 from "$lib/assets/albums/asturias/f.jpeg";
-import canal1 from "$lib/assets/albums/canal/1.jpeg";
-import canal2 from "$lib/assets/albums/canal/2.jpeg";
-import canal3 from "$lib/assets/albums/canal/3.jpeg";
-import canal4 from "$lib/assets/albums/canal/4.jpeg";
-import canal5 from "$lib/assets/albums/canal/5.jpeg";
-import canal6 from "$lib/assets/albums/canal/6.jpeg";
-import canal7 from "$lib/assets/albums/canal/7.jpeg";
-import canal8 from "$lib/assets/albums/canal/8.jpeg";
-import canal9 from "$lib/assets/albums/canal/9.jpeg";
-import canal10 from "$lib/assets/albums/canal/10.jpeg";
-import canal11 from "$lib/assets/albums/canal/11.jpeg";
-import canal12 from "$lib/assets/albums/canal/12.jpeg";
-import canal13 from "$lib/assets/albums/canal/13.jpeg";
-import canal14 from "$lib/assets/albums/canal/14.jpeg";
-import canal15 from "$lib/assets/albums/canal/15.jpeg";
-import canal16 from "$lib/assets/albums/canal/16.jpeg";
-import canal17 from "$lib/assets/albums/canal/17.jpeg";
-import canal18 from "$lib/assets/albums/canal/18.jpeg";
-import canal19 from "$lib/assets/albums/canal/19.jpeg";
-import canal20 from "$lib/assets/albums/canal/20.jpeg";
-import canal21 from "$lib/assets/albums/canal/21.jpeg";
-import canal22 from "$lib/assets/albums/canal/22.jpeg";
-import canal23 from "$lib/assets/albums/canal/23.jpeg";
-import canal24 from "$lib/assets/albums/canal/24.jpeg";
-import canal25 from "$lib/assets/albums/canal/25.jpeg";
-import canal26 from "$lib/assets/albums/canal/26.jpeg";
-import canal27 from "$lib/assets/albums/canal/27.jpeg";
-import canal28 from "$lib/assets/albums/canal/28.jpeg";
-import canal29 from "$lib/assets/albums/canal/29.jpeg";
-import canal30 from "$lib/assets/albums/canal/30.jpeg";
-import canal31 from "$lib/assets/albums/canal/31.jpeg";
-import canal32 from "$lib/assets/albums/canal/32.jpeg";
-import canal33 from "$lib/assets/albums/canal/33.jpeg";
-import canal34 from "$lib/assets/albums/canal/34.jpeg";
-import canal35 from "$lib/assets/albums/canal/35.jpeg";
-import jardin1 from "$lib/assets/albums/jardin/1.jpeg";
-import jardin2 from "$lib/assets/albums/jardin/2.jpeg";
-import jardin3 from "$lib/assets/albums/jardin/3.jpeg";
-import jardin4 from "$lib/assets/albums/jardin/4.jpeg";
-import jardin5 from "$lib/assets/albums/jardin/5.jpeg";
-import jardin6 from "$lib/assets/albums/jardin/6.jpeg";
-import jardin7 from "$lib/assets/albums/jardin/7.jpeg";
-import jardin8 from "$lib/assets/albums/jardin/8.jpeg";
-import newyork1 from "$lib/assets/albums/nyc/1.jpeg";
-import newyork2 from "$lib/assets/albums/nyc/2.jpeg";
-import newyork3 from "$lib/assets/albums/nyc/3.jpeg";
-import newyork4 from "$lib/assets/albums/nyc/4.jpeg";
-import newyork5 from "$lib/assets/albums/nyc/5.jpeg";
-import newyork6 from "$lib/assets/albums/nyc/6.jpeg";
-import yosemite1 from "$lib/assets/albums/yosemite/1.jpeg";
-import yosemite2 from "$lib/assets/albums/yosemite/2.jpeg";
-import yosemite3 from "$lib/assets/albums/yosemite/3.jpeg";
-import yosemite4 from "$lib/assets/albums/yosemite/4.jpeg";
-import yosemite5 from "$lib/assets/albums/yosemite/5.jpeg";
-import yosemite6 from "$lib/assets/albums/yosemite/6.jpeg";
-import yosemite7 from "$lib/assets/albums/yosemite/7.jpeg";
-import yosemite8 from "$lib/assets/albums/yosemite/8.jpeg";
-import yosemite9 from "$lib/assets/albums/yosemite/9.jpeg";
-import yosemite10 from "$lib/assets/albums/yosemite/10.jpeg";
-import yosemite11 from "$lib/assets/albums/yosemite/11.jpeg";
-import yosemite12 from "$lib/assets/albums/yosemite/12.jpeg";
-import yosemite13 from "$lib/assets/albums/yosemite/13.jpeg";
-import yosemite14 from "$lib/assets/albums/yosemite/14.jpeg";
+import canal1 from "$lib/assets/albums/canal/a.jpeg";
+import canal2 from "$lib/assets/albums/canal/b.jpeg";
+import canal3 from "$lib/assets/albums/canal/c.jpeg";
+import canal4 from "$lib/assets/albums/canal/d.jpeg";
+import canal5 from "$lib/assets/albums/canal/e.jpeg";
+import canal6 from "$lib/assets/albums/canal/f.jpeg";
+import canal7 from "$lib/assets/albums/canal/g.jpeg";
+import canal8 from "$lib/assets/albums/canal/h.jpeg";
+import canal9 from "$lib/assets/albums/canal/i.jpeg";
+import canal10 from "$lib/assets/albums/canal/j.jpeg";
+import canal11 from "$lib/assets/albums/canal/k.jpeg";
+import canal12 from "$lib/assets/albums/canal/l.jpeg";
+import canal13 from "$lib/assets/albums/canal/m.jpeg";
+import canal14 from "$lib/assets/albums/canal/n.jpeg";
+import canal15 from "$lib/assets/albums/canal/o.jpeg";
+import canal16 from "$lib/assets/albums/canal/p.jpeg";
+import canal17 from "$lib/assets/albums/canal/q.jpeg";
+import canal18 from "$lib/assets/albums/canal/r.jpeg";
+import canal19 from "$lib/assets/albums/canal/s.jpeg";
+import canal20 from "$lib/assets/albums/canal/t.jpeg";
+import canal21 from "$lib/assets/albums/canal/u.jpeg";
+import canal22 from "$lib/assets/albums/canal/v.jpeg";
+import canal23 from "$lib/assets/albums/canal/w.jpeg";
+import canal24 from "$lib/assets/albums/canal/x.jpeg";
+import canal25 from "$lib/assets/albums/canal/y.jpeg";
+import canal26 from "$lib/assets/albums/canal/z.jpeg";
+import canal27 from "$lib/assets/albums/canal/aa.jpeg";
+import canal28 from "$lib/assets/albums/canal/bb.jpeg";
+import canal29 from "$lib/assets/albums/canal/cc.jpeg";
+import canal30 from "$lib/assets/albums/canal/dd.jpeg";
+import canal31 from "$lib/assets/albums/canal/ee.jpeg";
+import canal32 from "$lib/assets/albums/canal/ff.jpeg";
+import canal33 from "$lib/assets/albums/canal/gg.jpeg";
+import canal34 from "$lib/assets/albums/canal/hh.jpeg";
+import canal35 from "$lib/assets/albums/canal/ii.jpeg";
+import jardin1 from "$lib/assets/albums/jardin/a.jpeg";
+import jardin2 from "$lib/assets/albums/jardin/b.jpeg";
+import jardin3 from "$lib/assets/albums/jardin/c.jpeg";
+import jardin4 from "$lib/assets/albums/jardin/c.jpeg";
+import jardin5 from "$lib/assets/albums/jardin/e.jpeg";
+import jardin6 from "$lib/assets/albums/jardin/f.jpeg";
+import jardin7 from "$lib/assets/albums/jardin/g.jpeg";
+import jardin8 from "$lib/assets/albums/jardin/h.jpeg";
+import newyork1 from "$lib/assets/albums/nyc/a.jpeg";
+import newyork2 from "$lib/assets/albums/nyc/b.jpeg";
+import newyork3 from "$lib/assets/albums/nyc/c.jpeg";
+import newyork4 from "$lib/assets/albums/nyc/d.jpeg";
+import newyork5 from "$lib/assets/albums/nyc/e.jpeg";
+import newyork6 from "$lib/assets/albums/nyc/f.jpeg";
+import yosemite1 from "$lib/assets/albums/yosemite/a.jpeg";
+import yosemite2 from "$lib/assets/albums/yosemite/b.jpeg";
+import yosemite3 from "$lib/assets/albums/yosemite/c.jpeg";
+import yosemite4 from "$lib/assets/albums/yosemite/d.jpeg";
+import yosemite5 from "$lib/assets/albums/yosemite/e.jpeg";
+import yosemite6 from "$lib/assets/albums/yosemite/f.jpeg";
+import yosemite7 from "$lib/assets/albums/yosemite/g.jpeg";
+import yosemite8 from "$lib/assets/albums/yosemite/h.jpeg";
+import yosemite9 from "$lib/assets/albums/yosemite/i.jpeg";
+import yosemite10 from "$lib/assets/albums/yosemite/j.jpeg";
+import yosemite11 from "$lib/assets/albums/yosemite/k.jpeg";
+import yosemite12 from "$lib/assets/albums/yosemite/l.jpeg";
+import yosemite13 from "$lib/assets/albums/yosemite/m.jpeg";
+import yosemite14 from "$lib/assets/albums/yosemite/n.jpeg";
 
 const pics = [
-  {id: "asturias1", src: asturias1, name: "asturias1", albums:["asturias"]},
-  {id: "asturias2", src: asturias2, name: "asturias2", albums:["asturias"]},
-  {id: "asturias3", src: asturias3, name: "asturias3", albums:["asturias"]},
-  {id: "asturias4", src: asturias4, name: "asturias4", albums:["asturias"]},
-  {id: "asturias5", src: asturias5, name: "asturias5", albums:["asturias"]},
-  {id: "Asturias6", src: asturias6, name: "asturias6", albums:["asturias"]},
-  {id: "canal1", src: canal1, name: "canal1", albums:["canal"]},
-  {id: "canal2", src: canal2, name: "canal2", albums:["canal"]},
-  {id: "canal3", src: canal3, name: "canal3", albums:["canal"]},
-  {id: "canal4", src: canal4, name: "canal4", albums:["canal"]},
-  {id: "canal5", src: canal5, name: "canal5", albums:["canal"]},
-  {id: "canal6", src: canal6, name: "canal6", albums:["canal"]},
-  {id: "canal7", src: canal7, name: "canal7", albums:["canal"]},
-  {id: "canal8", src: canal8, name: "canal8", albums:["canal"]},
-  {id: "canal9", src: canal9, name: "canal9", albums:["canal"]},
-  {id: "canal10", src: canal10, name: "canal10", albums:["canal"]},
-  {id: "canal11", src: canal11, name: "canal11", albums:["canal"]},
-  {id: "canal12", src: canal12, name: "canal12", albums:["canal"]},
-  {id: "canal13", src: canal13, name: "canal13", albums:["canal"]},
-  {id: "canal14", src: canal14, name: "canal14", albums:["canal"]},
-  {id: "canal15", src: canal15, name: "canal15", albums:["canal"]},
-  {id: "canal16", src: canal16, name: "canal16", albums:["canal"]},
-  {id: "canal17", src: canal17, name: "canal17", albums:["canal"]},
-  {id: "canal18", src: canal18, name: "canal18", albums:["canal"]},
-  {id: "canal19", src: canal19, name: "canal19", albums:["canal"]},
-  {id: "canal20", src: canal20, name: "canal20", albums:["canal"]},
-  {id: "canal21", src: canal21, name: "canal21", albums:["canal"]},
-  {id: "canal22", src: canal22, name: "canal22", albums:["canal"]},
-  {id: "canal23", src: canal23, name: "canal23", albums:["canal"]},
-  {id: "canal24", src: canal24, name: "canal24", albums:["canal"]},
-  {id: "canal25", src: canal25, name: "canal25", albums:["canal"]},
-  {id: "canal26", src: canal26, name: "canal26", albums:["canal"]},
-  {id: "canal27", src: canal27, name: "canal27", albums:["canal"]},
-  {id: "canal28", src: canal28, name: "canal28", albums:["canal"]},
-  {id: "canal29", src: canal29, name: "canal29", albums:["canal"]},
-  {id: "canal30", src: canal30, name: "canal30", albums:["canal"]},
-  {id: "canal31", src: canal31, name: "canal31", albums:["canal"]},
-  {id: "canal32", src: canal32, name: "canal32", albums:["canal"]},
-  {id: "canal33", src: canal33, name: "canal33", albums:["canal"]},
-  {id: "canal34", src: canal34, name: "canal34", albums:["canal"]},
-  {id: "canal35", src: canal35, name: "canal35", albums:["canal"]},
-  {id: "jardin1", src: jardin1, name: "jardin1", albums:["jardin"]},
-  {id: "jardin2", src: jardin2, name: "jardin2", albums:["jardin"]},
-  {id: "jardin3", src: jardin3, name: "jardin3", albums:["jardin"]},
-  {id: "jardin4", src: jardin4, name: "jardin4", albums:["jardin"]},
-  {id: "jardin5", src: jardin5, name: "jardin5", albums:["jardin"]},
-  {id: "jardin6", src: jardin6, name: "jardin6", albums:["jardin"]},
-  {id: "jardin7", src: jardin7, name: "jardin7", albums:["jardin"]},
-  {id: "jardin8", src: jardin8, name: "jardin8", albums:["jardin"]},
-  {id: "newYork1", src: newyork1, name: "newYork1", albums:["newYork"]},
-  {id: "newYork2", src: newyork2, name: "newYork2", albums:["newYork"]},
-  {id: "newYork3", src: newyork3, name: "newYork3", albums:["newYork"]},
-  {id: "newYork4", src: newyork4, name: "newYork4", albums:["newYork"]},
-  {id: "newYork5", src: newyork5, name: "newYork5", albums:["newYork"]},
-  {id: "newYork6", src: newyork6, name: "newYork6", albums:["newYork"]},
-  {id: "yosemite1", src: yosemite1, name: "yosemite1", albums:["yosemite"]},
-  {id: "yosemite2", src: yosemite2, name: "yosemite2", albums:["yosemite"]},
-  {id: "yosemite3", src: yosemite3, name: "yosemite3", albums:["yosemite"]},
-  {id: "yosemite4", src: yosemite4, name: "yosemite4", albums:["yosemite"]},
-  {id: "yosemite5", src: yosemite5, name: "yosemite5", albums:["yosemite"]},
-  {id: "yosemite6", src: yosemite6, name: "yosemite6", albums:["yosemite"]},
-  {id: "yosemite7", src: yosemite7, name: "yosemite7", albums:["yosemite"]},
-  {id: "yosemite8", src: yosemite8, name: "yosemite8", albums:["yosemite"]},
-  {id: "yosemite9", src: yosemite9, name: "yosemite9", albums:["yosemite"]},
-  {id: "yosemite10", src: yosemite10, name: "yosemite10", albums:["yosemite"]},
-  {id: "yosemite11", src: yosemite11, name: "yosemite11", albums:["yosemite"]},
-  {id: "yosemite12", src: yosemite12, name: "yosemite12", albums:["yosemite"]},
-  {id: "yosemite13", src: yosemite13, name: "yosemite13", albums:["yosemite"]},
-  {id: "yosemite14", src: yosemite14, name: "yosemite14", albums:["yosemite"]},
+  { id: "asturias1", src: asturias1, name: "asturias1", albums: ["asturias"] },
+  { id: "asturias2", src: asturias2, name: "asturias2", albums: ["asturias"] },
+  { id: "asturias3", src: asturias3, name: "asturias3", albums: ["asturias"] },
+  { id: "asturias4", src: asturias4, name: "asturias4", albums: ["asturias"] },
+  { id: "asturias5", src: asturias5, name: "asturias5", albums: ["asturias"] },
+  { id: "Asturias6", src: asturias6, name: "asturias6", albums: ["asturias"] },
+  { id: "canal1", src: canal1, name: "canal1", albums: ["canal"] },
+  { id: "canal2", src: canal2, name: "canal2", albums: ["canal"] },
+  { id: "canal3", src: canal3, name: "canal3", albums: ["canal"] },
+  { id: "canal4", src: canal4, name: "canal4", albums: ["canal"] },
+  { id: "canal5", src: canal5, name: "canal5", albums: ["canal"] },
+  { id: "canal6", src: canal6, name: "canal6", albums: ["canal"] },
+  { id: "canal7", src: canal7, name: "canal7", albums: ["canal"] },
+  { id: "canal8", src: canal8, name: "canal8", albums: ["canal"] },
+  { id: "canal9", src: canal9, name: "canal9", albums: ["canal"] },
+  { id: "canal10", src: canal10, name: "canal10", albums: ["canal"] },
+  { id: "canal11", src: canal11, name: "canal11", albums: ["canal"] },
+  { id: "canal12", src: canal12, name: "canal12", albums: ["canal"] },
+  { id: "canal13", src: canal13, name: "canal13", albums: ["canal"] },
+  { id: "canal14", src: canal14, name: "canal14", albums: ["canal"] },
+  { id: "canal15", src: canal15, name: "canal15", albums: ["canal"] },
+  { id: "canal16", src: canal16, name: "canal16", albums: ["canal"] },
+  { id: "canal17", src: canal17, name: "canal17", albums: ["canal"] },
+  { id: "canal18", src: canal18, name: "canal18", albums: ["canal"] },
+  { id: "canal19", src: canal19, name: "canal19", albums: ["canal"] },
+  { id: "canal20", src: canal20, name: "canal20", albums: ["canal"] },
+  { id: "canal21", src: canal21, name: "canal21", albums: ["canal"] },
+  { id: "canal22", src: canal22, name: "canal22", albums: ["canal"] },
+  { id: "canal23", src: canal23, name: "canal23", albums: ["canal"] },
+  { id: "canal24", src: canal24, name: "canal24", albums: ["canal"] },
+  { id: "canal25", src: canal25, name: "canal25", albums: ["canal"] },
+  { id: "canal26", src: canal26, name: "canal26", albums: ["canal"] },
+  { id: "canal27", src: canal27, name: "canal27", albums: ["canal"] },
+  { id: "canal28", src: canal28, name: "canal28", albums: ["canal"] },
+  { id: "canal29", src: canal29, name: "canal29", albums: ["canal"] },
+  { id: "canal30", src: canal30, name: "canal30", albums: ["canal"] },
+  { id: "canal31", src: canal31, name: "canal31", albums: ["canal"] },
+  { id: "canal32", src: canal32, name: "canal32", albums: ["canal"] },
+  { id: "canal33", src: canal33, name: "canal33", albums: ["canal"] },
+  { id: "canal34", src: canal34, name: "canal34", albums: ["canal"] },
+  { id: "canal35", src: canal35, name: "canal35", albums: ["canal"] },
+  { id: "jardin1", src: jardin1, name: "jardin1", albums: ["jardin"] },
+  { id: "jardin2", src: jardin2, name: "jardin2", albums: ["jardin"] },
+  { id: "jardin3", src: jardin3, name: "jardin3", albums: ["jardin"] },
+  { id: "jardin4", src: jardin4, name: "jardin4", albums: ["jardin"] },
+  { id: "jardin5", src: jardin5, name: "jardin5", albums: ["jardin"] },
+  { id: "jardin6", src: jardin6, name: "jardin6", albums: ["jardin"] },
+  { id: "jardin7", src: jardin7, name: "jardin7", albums: ["jardin"] },
+  { id: "jardin8", src: jardin8, name: "jardin8", albums: ["jardin"] },
+  { id: "newYork1", src: newyork1, name: "newYork1", albums: ["newYork"] },
+  { id: "newYork2", src: newyork2, name: "newYork2", albums: ["newYork"] },
+  { id: "newYork3", src: newyork3, name: "newYork3", albums: ["newYork"] },
+  { id: "newYork4", src: newyork4, name: "newYork4", albums: ["newYork"] },
+  { id: "newYork5", src: newyork5, name: "newYork5", albums: ["newYork"] },
+  { id: "newYork6", src: newyork6, name: "newYork6", albums: ["newYork"] },
+  { id: "yosemite1", src: yosemite1, name: "yosemite1", albums: ["yosemite"] },
+  { id: "yosemite2", src: yosemite2, name: "yosemite2", albums: ["yosemite"] },
+  { id: "yosemite3", src: yosemite3, name: "yosemite3", albums: ["yosemite"] },
+  { id: "yosemite4", src: yosemite4, name: "yosemite4", albums: ["yosemite"] },
+  { id: "yosemite5", src: yosemite5, name: "yosemite5", albums: ["yosemite"] },
+  { id: "yosemite6", src: yosemite6, name: "yosemite6", albums: ["yosemite"] },
+  { id: "yosemite7", src: yosemite7, name: "yosemite7", albums: ["yosemite"] },
+  { id: "yosemite8", src: yosemite8, name: "yosemite8", albums: ["yosemite"] },
+  { id: "yosemite9", src: yosemite9, name: "yosemite9", albums: ["yosemite"] },
+  {
+    id: "yosemite10",
+    src: yosemite10,
+    name: "yosemite10",
+    albums: ["yosemite"],
+  },
+  {
+    id: "yosemite11",
+    src: yosemite11,
+    name: "yosemite11",
+    albums: ["yosemite"],
+  },
+  {
+    id: "yosemite12",
+    src: yosemite12,
+    name: "yosemite12",
+    albums: ["yosemite"],
+  },
+  {
+    id: "yosemite13",
+    src: yosemite13,
+    name: "yosemite13",
+    albums: ["yosemite"],
+  },
+  {
+    id: "yosemite14",
+    src: yosemite14,
+    name: "yosemite14",
+    albums: ["yosemite"],
+  },
 ];
-
 
 export default pics;
